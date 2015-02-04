25de5b7b9fe721b275965baa9125b2c4cd7f6ac3
bi3mer <colan@comcast.net>
2015-02-04 00:30:55 -0500

    spawn point groundwork
    I started implementing the spawners so they move around the map. Not
    fully functional in terms of moving around. But coming close.

---
+++ Assets/Scripts/Enemy/MoveSpawner.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSpawner : MonoBehaviour {
+
+	public GameObject spawnPoint_One;
+	public GameObject spawnPoint_Two;
+	public GameObject spawnPoint_Three;
+	public GameObject spawnPoint_Four;
+	public float fracJourney;
+
+	private GameObject previousTarget;
+	private GameObject target;
+
+	void Start(){
+		previousTarget = spawnPoint_Four; //this should probably be changed!!!!
+		target = spawnPoint_One;
+		move ();
+	}
+
+	void move(){
+		transform.position = Vector3.Lerp(previousTarget.transform.position, target.transform.position, Time.deltaTime * fracJourney);
+	}
+
+	void Update () {
+		// working on this part, the idea is to have it move 
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "spawn_point") {
+			switchTarget();		
+		}
+	} 
+
+	void switchTarget(){
+		// Function will switch target spawnPoint when the current target is reached
+		// Switch statement is probably better here
+		if (target == this.spawnPoint_One) {
+			previousTarget = this.spawnPoint_One;
+			target = this.spawnPoint_Two;		
+		} else if(target == this.spawnPoint_Two){
+			previousTarget = this.spawnPoint_Two;
+			target = this.spawnPoint_Three;
+		} else if(target == this.spawnPoint_Three){
+			previousTarget = this.spawnPoint_Three;
+			target = this.spawnPoint_Four;
+		} else if(target == this.spawnPoint_Four){
+			previousTarget = this.spawnPoint_Four;
+			target = this.spawnPoint_One;
+		} else{
+			Debug.Log("Error: switchTarget was invalid. Auto-corrected to spawPoint_One.");
+			previousTarget = this.spawnPoint_Four;
+			target = this.spawnPoint_One;
+		}
+		move ();
+	}
+}
